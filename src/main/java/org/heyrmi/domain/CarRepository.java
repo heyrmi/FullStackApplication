bd4fbeb94eaed8960df12df31e3a373e1ef6f055
Rahul Mishra <rahulmishra.kv@gmail.com>
2022-09-04 15:40:33 +0530

    Added: Search Filter

--- src/main/java/org/heyrmi/domain/CarRepository.java
+++ src/main/java/org/heyrmi/domain/CarRepository.java
@@ -1,6 +1,16 @@
 package org.heyrmi.domain;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-public interface CarRepository extends CrudRepository<Car, Long> {
+import java.util.List;
+
+@RepositoryRestResource
+public interface CarRepository extends CrudRepository<Car, Long>  {
+    // Fetch cars by brand
+    List<Car> findByBrand(@Param("brand") String brand);
+
+    // Fetch cars by color
+    List<Car> findByColor(@Param("color") String color);
 }
--- src/main/java/org/heyrmi/web/CarController.java
+++ src/main/java/org/heyrmi/web/CarController.java
@@ -5,6 +5,7 @@ import org.heyrmi.domain.Car;
 import org.heyrmi.domain.CarRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -14,7 +15,7 @@ public class CarController {
     @Autowired
     private CarRepository carRepository;
 
-    @GetMapping(value="/cars")
+    @RequestMapping(value="/cars")
     public Iterable<Car> getCars() {
         return carRepository.findAll();
     }
