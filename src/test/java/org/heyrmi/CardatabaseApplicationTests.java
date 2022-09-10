6c5810652148263cc0b92a3a11576c52d045b8ab
Rahul Mishra <rahulmishra.kv@gmail.com>
2022-09-11 03:12:32 +0530

    Added: Unit Test

---
+++ src/test/java/org/heyrmi/CardatabaseApplicationTests.java
@@ -0,0 +1,20 @@
+package org.heyrmi;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import org.heyrmi.web.CarController;
+
+@SpringBootTest
+class CardatabaseApplicationTests {
+	@Autowired
+	private CarController controller;
+
+	@Test
+	void contextLoads() {
+		assertThat(controller).isNotNull();
+	}
+}
--- src/test/java/org/heyrmi/FullStackApplicationTests.java
+++ src/test/java/org/heyrmi/FullStackApplicationTests.java
@@ -2,32 +2,21 @@ package org.heyrmi;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-
-import org.heyrmi.domain.User;
-import org.heyrmi.domain.UserRepository;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
 
+import org.springframework.test.annotation.Rollback;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = Replace.NONE)
 @Rollback(false)
 class FullStackApplicationTests {
 
-
-
-
-    @Test
+    @Test()
     public void testCreateFirstRole() {
-
 
     }
 }
-
-
