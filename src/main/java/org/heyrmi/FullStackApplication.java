39d0c8b89bdd21166898baef9c5a226fc72821ac
Rahul Mishra <rahulmishra.kv@gmail.com>
2022-10-30 09:55:15 +0530

    Updated: CI

--- src/main/java/org/heyrmi/FullStackApplication.java
+++ src/main/java/org/heyrmi/FullStackApplication.java
@@ -11,7 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.util.Arrays;
 
 @SpringBootApplication
-public class FullStackApplication {
+public class FullStackApplication implements CommandLineRunner{
 	private static final Logger logger = LoggerFactory.getLogger(FullStackApplication.class);
 
 	@Autowired
@@ -30,39 +30,39 @@ public class FullStackApplication {
 	// To create tables before program execution
 	// This also destroys tables once program exits
 
-	// @Override
-	// public void run(String... args) throws Exception {
-	// // Add owner objects and save these to db
-	// Owner owner1 = new Owner("John", "Johnson");
-	// Owner owner2 = new Owner("Mary", "Robinson");
-	// orepository.saveAll(Arrays.asList(owner1, owner2));
-	// for (Owner owner : orepository.findAll()) {
-	// logger.info(owner.getFirstname() + " " + owner.getLastname());
-	// }
+	 @Override
+	 public void run(String... args) throws Exception {
+	 // Add owner objects and save these to db
+	 Owner owner1 = new Owner("John", "Johnson");
+	 Owner owner2 = new Owner("Mary", "Robinson");
+	 orepository.saveAll(Arrays.asList(owner1, owner2));
+	 for (Owner owner : orepository.findAll()) {
+	 logger.info(owner.getFirstname() + " " + owner.getLastname());
+	 }
 
-	// // Add car object and link to owners and save these to db
-	// Car car1 = new Car("Ford", "Mustang", "Red",
-	// "ADF-1121", 2021, 59000, owner1);
-	// Car car2 = new Car("Nissan", "Leaf", "White",
-	// "SSJ-3002", 2019, 29000, owner2);
-	// Car car3 = new Car("Toyota", "Prius", "Silver",
-	// "KKO-0212", 2020, 39000, owner2);
-	// Car car4 = new Car("Audi", "A4", "Black",
-	// "GJJ-1245", 2021, 99000, owner1);
-	// repository.saveAll(Arrays.asList(car1, car2, car3, car4));
+	 // Add car object and link to owners and save these to db
+	 Car car1 = new Car("Ford", "Mustang", "Red",
+	 "ADF-1121", 2021, 59000, owner1);
+	 Car car2 = new Car("Nissan", "Leaf", "White",
+	 "SSJ-3002", 2019, 29000, owner2);
+	 Car car3 = new Car("Toyota", "Prius", "Silver",
+	 "KKO-0212", 2020, 39000, owner2);
+	 Car car4 = new Car("Audi", "A4", "Black",
+	 "GJJ-1245", 2021, 99000, owner1);
+	 repository.saveAll(Arrays.asList(car1, car2, car3, car4));
 
-	// for (Car car : repository.findAll()) {
-	// logger.info(car.getBrand() + " " + car.getModel());
-	// }
+	 for (Car car : repository.findAll()) {
+	 logger.info(car.getBrand() + " " + car.getModel());
+	 }
 
-	// urepository.save(new User("user",
-	// "$2a$10$NVM0n8ElaRgg7zWO1CxUdei7vWoPg91Lz2aYavh9.f9q0e4bRadue", "USER"));
-	// urepository.save(new User("admin",
-	// "$2a$10$8cjz47bjbR4Mn8GMg9IZx.vyjhLXR/SKKMSZ9.mP9vpMu0ssKi8GW", "ADMIN"));
-	// for (User user : urepository.findAll()) {
-	// logger.info(user.getUsername() + " " + user.getRole());
-	// }
+	 urepository.save(new User("user",
+	 "$2a$10$NVM0n8ElaRgg7zWO1CxUdei7vWoPg91Lz2aYavh9.f9q0e4bRadue", "USER"));
+	 urepository.save(new User("admin",
+	 "$2a$10$8cjz47bjbR4Mn8GMg9IZx.vyjhLXR/SKKMSZ9.mP9vpMu0ssKi8GW", "ADMIN"));
+	 for (User user : urepository.findAll()) {
+	 logger.info(user.getUsername() + " " + user.getRole());
+	 }
 
-	// }
+	 }
 
 }
