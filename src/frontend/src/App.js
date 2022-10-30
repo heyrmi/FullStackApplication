7bf65511732e1b792d310de702c88381034e01e3
Rahul Mishra <rahulmishra.kv@gmail.com>
2022-10-30 10:06:08 +0530

    Minor Changes to Build

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Carlist from './components/Carlist'
 import Login from './components/Login'
 
 function App() {
